f50b1c9a121308fa20f5b747c4d176cd3e286a5f
wiadiwon <seoyoung.john@gm.gist.ac.kr>
2024-11-25 21:42:09 +0900

    Update. model

--- pose2unity.py
+++ pose2unity.py
@@ -4,9 +4,9 @@ import json
 import numpy as np
 
 
-from model import Tactile2PoseFeatureModel, SpatialSoftmax3D
+from model import Tactile2PoseFeatureModel, Tactile2PoseVRHeatmap, Tactile2PoseVRLinear, SpatialSoftmax3D
 from config import Tactile2PoseConfig
-from dataloader import get_tactile_dataloaders
+from dataloader import get_tactile_dataloaders, generate_heatmap
 import const
 import torch
 
@@ -14,13 +14,18 @@ HOST = "127.0.0.1"
 PORT = 12345
 
 
-def load_model(config, device="cuda"):
-    model = Tactile2PoseFeatureModel(config)
+def load_model(config, model_path, device="cuda"):
+    model_dict ={
+        "Tactile2PoseFeatureModel": Tactile2PoseFeatureModel,
+        "Tactile2PoseVRHeatmap": Tactile2PoseVRHeatmap,
+        "Tactile2PoseVRLinear": Tactile2PoseVRLinear
+    }
+    
+    model = model_dict[config.MODEL](config)
     model.to(device)
     softmax = SpatialSoftmax3D(20, 20, 18, 19)
     softmax.to(device)
     
-    model_path = ".\\models\\best_model_weighted_mse_loss.pth"
     #load model
     try:
         model.load_state_dict(torch.load(model_path).state_dict())
@@ -30,16 +35,21 @@ def load_model(config, device="cuda"):
 
     return model, softmax
 
-def get_pose_aciton_data(data_iter, model, softmax, device="cuda"):
-    
-    data = next(data_iter)
+def get_pose_aciton_data(config, data, model, softmax, device="cuda"):
+        
     input_tac_left, input_tac_right, input_kp, action_idx = data    
     
     tactile_left = input_tac_left.to(device).float()
     tactile_right = input_tac_right.to(device).float()
-    keypoint_vr = input_kp[:,:,const.VR_INDEXS, :].to(device).float()
     
-    heatmap_pred = model(tactile_left, tactile_right, keypoint_vr)
+    if config.MODEL == "Tactile2PoseVRHeatmap":
+        keypoint_vr = input_kp[:,-1, const.VR_INDEXS, :].to(device).float()
+        vr_keypoint_heatmap = generate_heatmap(keypoint_vr)
+        heatmap_pred = model(tactile_left, tactile_right, vr_keypoint_heatmap)
+    
+    else:
+        keypoint_vr = input_kp[:,:,const.VR_INDEXS, :].to(device).float()
+        heatmap_pred = model(tactile_left, tactile_right, keypoint_vr)
     heatmap_pred = torch.clip(heatmap_pred, 0, None)
     keypoint_out, _ = softmax(heatmap_pred)
     
@@ -55,7 +65,7 @@ def denormalize_keypoints(normalized_keypoints):
     restored_keypoints = normalized_keypoints.clone()
     restored_keypoints[:,:2] -= 0.5
     restored_keypoints *= 2.0
-    # x좌표 뒤집기
+    # x,y좌표 뒤집기
     restored_keypoints[:,0] *= -1
     restored_keypoints[:,1] *= -1
     return restored_keypoints
@@ -75,9 +85,15 @@ def main():
     #load model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"device: {device}")
-    model, softmax = load_model(config, device)
     
-    model_output_data = get_pose_aciton_data(test_data_iterator, model, softmax, device)
+    # config.MODEL = "Tactile2PoseFeatureModel"
+    config.MODEL = "Tactile2PoseVRHeatmap"
+    # config.MODEL = "Tactile2PoseVRLinear"
+    model_path = ".\\models\\best_model_heatmap_lowerbody_weighted.pth"
+    model, softmax = load_model(config, model_path, device)
+    
+    data = next(test_data_iterator)
+    model_output_data = get_pose_aciton_data(config, data, model, softmax, device)
     # start server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
@@ -94,10 +110,15 @@ def main():
                 
                 # unity_vr_data = json.loads(data.decode('utf-8'))
                 # print(f"Unity에서 받은 vr 좌표 데이터: {unity_vr_data}")
-                
-                model_output_data = get_pose_aciton_data(test_data_iterator, model, softmax, device)
+                try:
+                    data = next(test_data_iterator)
+                except StopIteration:
+                    test_data_iterator = iter(test_dataloader)
+                    data = next(test_data_iterator)
+                model_output_data = get_pose_aciton_data(config, data, model, softmax, device)
+
                 conn.sendall(json.dumps(model_output_data).encode('utf-8'))
-                print(f"Unity에 보낼 model 데이터: {model_output_data['keypoints'][11]}, {model_output_data['keypoints'][12]}")
+                print(f"Unity에 보낼 model 데이터: {model_output_data['keypoints'][0]}, {model_output_data['keypoints'][1]}")
                 
 
 if __name__ == "__main__":
